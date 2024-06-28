36ed177019606af5b7cfd83aa1061e4d7cf9f822
SimanyeSTS <simanyesomdaka@gmail.com>
2024-06-28 12:36:38 +0200

    ...

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <img src="https://simanyests.github.io/Images/
+images/IMG_1675202806226.jpg" alt="profile"
+                class="img-fluid shadow rounded-top" loading="lazy">
+        </div>
+        <div class="row justify-content-center">
+            <div v-if="about?.length">
+                <p class="lead" v-for="(self, index ) in about" :key="index">
+                    {{ self }}
+                </p>
+            </div>
+            <Spinner v-else />
+        </div>
+    </div>
+</template>
+
+<script setup>
+import Spinner from './Spinner.vue'
+import { computed, onMounted } from 'vue'
+import { useStore } from 'vuex'
+const store = useStore()
+const about =
+    computed(() => store.state.about)
+
+onMounted(() => {
+    store.dispatch('fetchAbout')
+})
+
+</script>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,40 +1,35 @@
+
 <template>
   <div class="container">
-
     <section class="row" id="landing">
       <LandingSection />
-      
     </section>
-
     <section class="row" id="about">
-
+      <AboutSection />
     </section>
-
     <section class="row" id="resume">
 
     </section>
-
     <section class="row" id="project">
 
     </section>
+    <section class="row" id="testimonials">
 
+    </section>
     <section class="row" id="reachMe">
 
     </section>
-
-    <section class="row" id="testimonials">
-
-</section>
-
   </div>
 </template>
 
 <script>
-import LandingSection from '@/components/LandingSection.vue';
+import LandingSection from '@/components/LandingSection.vue'
+import AboutSection from '@/components/AboutSection.vue'
 export default {
   name: 'HomeView',
   components: {
-    LandingSection
+    LandingSection,
+    AboutSection
   }
 }
 </script>
